4e1e16af1669aa9e28aa1af1627bb5b63cb4b576
sudheervk <sudheervk88@gmail.com>
2023-01-22 22:43:17 +0530

    extent report commit

---
+++ src/main/java/com/listeners/TestListener.java
@@ -0,0 +1,39 @@
+package com.listeners;
+
+import com.reports.ExtentLogger;
+import com.reports.ExtentReport;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener, ISuiteListener {
+
+    @Override
+    public void onStart(ISuite suite) {
+        ExtentReport.initReport();
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        ExtentReport.tearDownReport();
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentReport.createTest(result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ExtentLogger.pass(result.getTestName()+" is Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ExtentLogger.fail(String.valueOf(result.getThrowable()));
+    }
+
+
+
+}
